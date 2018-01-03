68aa7b103eba43868b2b1a2b9ed1257d4d32f080
cursist <cursist@LOK111-05>
2018-01-03 13:42:32 +0100

    Oefening 2: AantalKinderenTonen

--- src/main/java/be/vdab/PersoonViewer.java
+++ src/main/java/be/vdab/PersoonViewer.java
@@ -5,12 +5,20 @@ import java.util.List;
 import be.vdab.entities.Persoon;
 
 class PersoonViewer {
+	private boolean aantalKinderenTonen;
+	
+	public void setAantalKinderenTonen(boolean aantalKinderenTonen) {
+		this.aantalKinderenTonen = aantalKinderenTonen;
+	}
+
 	public void afbeelden(List<Persoon> personen) {
 		for (Persoon persoon : personen) {
 			System.out.println("PersoonNr: \t\t" + persoon.getPersoonNr());
 			System.out.println("Voornaam: \t\t" + persoon.getVoornaam());
 			System.out.println("Familienaam: \t\t" + persoon.getFamilienaam());
-			System.out.println("Aantal kinderen: \t" + persoon.getAantalKinderen());
+			if(aantalKinderenTonen) {
+				System.out.println("Aantal kinderen: \t" + persoon.getAantalKinderen());
+			}
 			System.out.println();
 		}
 	}
